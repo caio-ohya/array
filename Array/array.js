1f111dd35907796dd262c9d2cda2612669ef4781
caio-ohya <caio.e.lima@escola.pr.gov.br>
2026-03-16 09:09:27 -0300

    Add files via upload

---
+++ Array/array.js
@@ -0,0 +1,26 @@
+let lista = [10,"kkkkk",true,false,{nome:"Jorge"}];
+lista[3] = 70;
+lista.push("EU TO LOCO!!!")
+lista.shift();
+console.log(lista);
+
+let num = [10,20,30,40,50];
+for(let i = 0; i < num.length; i++){
+    console.log("Elemento "+num[i]);
+}
+
+let matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+for(let i = 0; i < matrix.length; i++){
+    for(let j = 0; j < matrix[i].length; j++){
+        console.log(matrix[i][j]);
+    }
+    console.log(matriz[0], [1])
+    console.log(matriz[1], [1])
+    console.log(matriz[2], [1])
+
+}
